a06e31844cfe1c325b817b9ecd9c3035167ed5cb
Rizame <igskade@gmail.com>
2025-05-03 08:59:30 +0200

    goal is now being saved

--- app.py
+++ app.py
@@ -434,7 +434,8 @@ class MainWindow(QMainWindow):
         # monthly_goal = amount_val / months_available
 
         goal_data = {"name": name, "amount": amount, "date": date}
-        goal_data.save("SavedGoal.json")
+        with open('SavedGoal.json', 'w') as f:
+            json.dump(goal_data, f)
 
         result_json = transaction_promt(name, amount, date, income)
         if result_json["is_goal_realistic"]:
